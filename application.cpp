ca3abd670798b9274de8f5342115eb8d1a8c2193
AlirezaM02 <alirezamirzaei68@gmail.com>
2021-07-02 15:28:30 +0430

    Add functions and attributes to application

--- application.cpp
+++ application.cpp
@@ -1,6 +1,10 @@
 #include "application.h"
 
-Application::Application()
-{
-
-}
+Application::Application(){}
+Application::~Application(){}
+void Application::savePlayersData(){}
+void Application::fetchPlayersData(){}
+Player Application::*findPlayer(QString username){}
+bool Application::login(QString username, QString password){}
+void Application::logout(){}
+void Application::setCurrentUser(int accID){}
--- application.h
+++ application.h
@@ -1,16 +1,24 @@
 #ifndef APPLICATION_H
 #define APPLICATION_H
+
 #include <QVector>
+#include <QString>
 #include "player.h"
+
 class Application
 {
 private:
     QVector <Player> playerlist;
     Player currentPlayer;
-
-
 public:
     Application();
+    ~Application();
+    void savePlayersData();
+    void fetchPlayersData();
+    Player *findPlayer(QString username);
+    bool login(QString username, QString password);
+    void logout();
+    void setCurrentUser(int accID);
 };
 
 #endif // APPLICATION_H
