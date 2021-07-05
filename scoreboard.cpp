04fb7bfa522371f1b3a08c91cd32335296c155ce
AlirezaM02 <alirezamirzaei68@gmail.com>
2021-07-05 15:06:54 +0430

    Implemented scoreboard class

--- scoreboard.cpp
+++ scoreboard.cpp
@@ -5,10 +5,59 @@ Scoreboard::Scoreboard(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Scoreboard)
 {
+    std::sort(players.begin(), players.end(), [](showPlayers p1, showPlayers p2)
+    {
+        if (p1.getLevel() > p2.getLevel()) return true;
+        else if (p1.getLevel() == p2.getLevel())
+            if (p1.getXP() > p2.getXP())
+                return true;
+        return false;
+    }
+    );
     ui->setupUi(this);
 }
 
 Scoreboard::~Scoreboard()
 {
     delete ui;
+}
+
+void showPlayers::setXP(int newXP)
+{
+    XP = newXP;
+}
+
+int showPlayers::getXP() const
+{
+    return XP;
+}
+
+void showPlayers::setID(int newID)
+{
+    ID = newID;
+}
+
+int showPlayers::getID() const
+{
+    return ID;
+}
+
+void showPlayers::setLevel(int newLevel)
+{
+    level = newLevel;
+}
+
+int showPlayers::getLevel() const
+{
+    return level;
+}
+
+void showPlayers::setName(const QString &newName)
+{
+    name = newName;
+}
+
+const QString &showPlayers::getName() const
+{
+    return name;
 }
--- scoreboard.h
+++ scoreboard.h
@@ -2,6 +2,33 @@
 #define SCOREBOARD_H
 
 #include <QDialog>
+#include <QVector>
+#include <algorithm>
+#include <vector>
+#include "player.h"
+using std::vector;
+
+class showPlayers
+{
+    int level, XP, ID;
+    QString name;
+public:
+    showPlayers(int _ID, QString _name, int _level, int _XP)
+    {
+        ID = _ID;
+        name = _name;
+        XP = _XP;
+        level = _level;
+    }
+    void setLevel(int newLevel);
+    void setXP(int newXP);
+    int getID() const;
+    void setID(int newID);
+    int getXP() const;
+    int getLevel() const;
+    const QString &getName() const;
+    void setName(const QString &newName);
+};
 
 namespace Ui {
 class Scoreboard;
@@ -12,11 +39,12 @@ class Scoreboard : public QDialog
     Q_OBJECT
 
 public:
-    explicit Scoreboard(QWidget *parent = nullptr);
+    explicit Scoreboard(QWidget *parent);
     ~Scoreboard();
 
 private:
     Ui::Scoreboard *ui;
+    QVector <showPlayers> players;
 };
 
 #endif // SCOREBOARD_H
